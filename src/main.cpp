fff91c300933298a723f84f8cf58676a37c04c28
paydownant <takase0211@gmail.com>
2024-09-11 20:37:57 +0900

    changed to satania

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char **argv) {
   }
   
   AUI *app = new AUI();
-  app->run("../images/yuno.png", stdout, 89);
+  app->run("../images/satania1.png", stdout, 89);
 
   fclose(ofp);
 }
